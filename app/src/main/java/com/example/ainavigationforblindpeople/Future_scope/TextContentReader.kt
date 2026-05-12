0fa5c544c3725756242fac3df23436c9633642d1
@24B11DS097 <srinivaskayala39@gmail.com>
2026-05-12 10:36:36 +0530

    Update package name in TextContentReader.kt

--- app/src/main/java/com/example/ainavigationforblindpeople/Future_scope/TextContentReader.kt
+++ app/src/main/java/com/example/ainavigationforblindpeople/Future_scope/TextContentReader.kt
@@ -1,4 +1,4 @@
-package com.yourpackage.name.future_scope
+package com.example.ainavigationforblindpeople
 
 /**
  * FUTURE SCOPE: This module is intended to handle OCR (Optical Character Recognition)
